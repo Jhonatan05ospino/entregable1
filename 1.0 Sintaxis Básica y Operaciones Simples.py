18f4ea45e528cd07b273ffe7e0306de555aecc1e
Jhonatan ospino <185928884+Jhonatan05ospino@users.noreply.github.com>
2024-12-03 09:25:06 -0500

    Add files via upload

---
+++ 1.0 Sintaxis Básica y Operaciones Simples.py
@@ -0,0 +1,18 @@
+# Imprimir un mensaje en la consola
+print("¡Hola, Realicemos una operacion matematica en Python!")
+
+# Declarar variables de diferentes tipos
+entero = 10
+flotante = 15.5
+cadena = "Hola Mundo"
+
+# Operaciones matemáticas simples
+suma = entero + flotante
+resta = flotante - entero
+multiplicacion = entero * 2
+division = flotante / 2
+
+# Concatenar cadenas de texto
+numero1 = input("Ingresa un numero del 1 al 10")
+numero2 = input("Ingresa un numero del 1 al 10")
+print(f"Hola ingresaste el numero  {numero1} y el numero {numero2}")
